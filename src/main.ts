e722e1d7644dc01517f6b248948204c88b303bb6
ZmjjKK <139601118+ocang21@users.noreply.github.com>
2025-02-17 22:25:54 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .setTitle('LATIHAN NESTJS KELAS 5B')
-    .setDescription('Muh.Dirham rahim - 105841105622')
+    .setDescription('AHMAD FAUZAN - 105841107522')
     .setVersion('0.1')
     .addTag('LATIHAN-1')
     .addBearerAuth()
